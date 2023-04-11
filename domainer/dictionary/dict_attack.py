76a0417856d912c67fa08a166e7b5e4de8ee06f2
delsyst0m <florianfirenzo@outlook.com>
2023-04-11 13:52:19 +0200

    fixed path

--- domainer/dictionary/dict_attack.py
+++ domainer/dictionary/dict_attack.py
@@ -32,8 +32,8 @@ class DictionaryAttack:
         elif self.strength == "4":
             strength = "1000000"
             
-        with open(f"{os.getcwd()}bitquark_20160227_subdomains \
-            _popular_{strength}.txt", "r", encoding="utf-8") as f:
+        with open(f"{os.getcwd()}\\wordlists\bitquark_20160227_subdomains" +
+            "_popular_{strength}.txt", "r", encoding="utf-8") as f:
             for line in f.readlines():
                 domains += [line.strip() + domain]
                 
